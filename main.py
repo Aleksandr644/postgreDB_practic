be351d1b43a5e962f09d20fdf2c8b9fe0d1a6efe
Aleksandr644 <ivanov2-91@mail.ru>
2024-03-07 11:48:29 +0500

    modified-2: main.py added OOP

--- main.py
+++ main.py
@@ -1,22 +1,36 @@
 import psycopg2
 from configparser import ConfigParser
 
-def config(filename:str = "db.ini", section:str = "postgresql") -> dict:
-    cfg = ConfigParser()
-    cfg.read(filename)
-    return dict(cfg.items(section))
 
-print("Connection")
-connection = psycopg2.connect(**config())
 
-var_cur = connection.cursor()
-
-print("Database version is -")
-var_cur.execute("SELECT version()")
-
-version_of_database = var_cur.fetchone()
-print(version_of_database)
-
-var_cur.close()
-connection.close()
-print("Database connection closed")
+class PGDB:
+    conn = None
+    cursor = None
+    def __init__(self, *, filename:str = "db.ini", section:str = "postgresql"):
+        PGDB.conn = psycopg2.connect(**PGDB.config(filename, section))
+        print("connection")
+        PGDB.cursor = PGDB.conn.cursor()
+        
+    def __del__(self):
+        print("Closing to connect")
+        if PGDB.cursor:
+            PGDB.cursor.close()
+            PGDB.cursor = None
+        if PGDB.conn:
+            PGDB.conn.close()
+            PGDB.conn = None
+    def request(execut:str = None):
+        if execut:
+            PGDB.cursor.execute(execut)
+        else:
+            PGDB.cursor.execut("SELECT version()")
+        return PGDB.cursor.fetchall()
+    @staticmethod
+    def config(filename:str ,section:str) -> dict:
+        cfg = ConfigParser()
+        cfg.read(filename)
+        return dict(cfg.items(section))
+if __name__ == "__main__":
+    db = PGDB()
+    print(db.request())
+    del db
