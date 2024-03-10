67ba1bda9c059fa3199fe50e6fe3082f0a35ed7d
Aleksandr644 <u9995844696@yandex.ru>
2024-03-10 14:29:16 +0500

    added exceptions app

--- module/connected_postgresql.py
+++ module/connected_postgresql.py
@@ -1,29 +1,31 @@
 import psycopg2
+from pathlib import Path
 from configparser import ConfigParser
 
 def singleton(cls):
     instances = {}
     def _singleton(*args, **kwargs):
-        print(args)
-        print(kwargs)
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
     return _singleton
-
 @singleton
 class PGDB:
-    def __init__(self, *, filename:str = "../config/db.ini", section:str = "postgresql"):
+    def __init__(self, *, filename:str = "config/db.ini", section:str = "postgresql"):
         self.conn= None
         self.cursor = None
         try:
-            self.conn = psycopg2.connect(**PGDB.config(filename, section))
-            print("Connection")
+            if not Path(filename).is_file():
+                raise FileNotFoundError(f"{'-'*30}\nФайла конфигурации './config/db.ini' не существует\nПоместите файл конфигурации db.ini в папку config\nСодержимое файла дожно содержать секцию {section}, которая должна содержать параметры подключения к базе данных\nнапример:\n[{section}]\nhost=localhost\nuser=user\npassword=password\n{'-'*30}")
+            print(f"Получаем данные из файла {filename}\n")
+            conf = self.config(filename, section)
+            self.conn = psycopg2.connect(**conf)
+            print("Подключаемся к базе данных")
             self.cursor = self.conn.cursor()
             params = self.conn.get_dsn_parameters()
-            print("information:")
+            print("Информация о подключении:")
             print(params)
-        except(Exception, psycopg2.Error) as error:
+        except Exception as error:
             print("Ошибка подключения к базе данных\n", error)
             exit()            
         
@@ -34,7 +36,7 @@ class PGDB:
         if self.conn:
             self.conn.close()
             self.conn = None
-        print("Closing to connect")
+        print("Отключено")
 
     def request(self, order:str) -> str:
         try:
@@ -45,13 +47,17 @@ class PGDB:
             print("Что то пошло не так...\n", error)
     
     @staticmethod
-    def config(filename:str ,section:str) -> dict:
-        cfg = ConfigParser()
-        cfg.read(filename)
-        return dict(cfg.items(section))
-  
+    def config(fname:str ,sctn:str) -> dict:
+        try:
+            cfg = ConfigParser()
+            cfg.read(fname)
+            return dict(cfg.items(sctn))
+        except Exception as error:
+            print(f"Не удалось получить параметры конфигурации\n {error}")
+            exit()
+    
 if __name__ == "__main__":
-    db = PGDB(filename= "../config/db.ini", section = "postgresql")
+    db = PGDB(filename= "config/db.ini", section = "postgresql")
     inputText = ""
     while True:
         inputText = input("Пожалуйста введите запрос:\nДля выхода введите exit\n")
