ff3063d7dfeb52bf40baa239539de66086515a3f
Aleksandr644 <u9995844696@yandex.ru>
2024-03-11 18:58:18 +0500

    added comments and config.examle file

--- module/connected_postgresql.py
+++ module/connected_postgresql.py
@@ -3,15 +3,27 @@ from pathlib import Path
 from configparser import ConfigParser
 
 def singleton(cls):
+    """
+    Декоратор класса для ограничения создания обьектов класса в 1 экземпляр.
+    При попытке создания нового экземпляра класса, возвращает уже созданный объект класса.
+    """
     instances = {}
     def _singleton(*args, **kwargs):
         if cls not in instances:
             instances[cls] = cls(*args, **kwargs)
         return instances[cls]
     return _singleton
+
 @singleton
 class PGDB:
+    """
+    Класс создания соединения с БД Postgre
+    """
     def __init__(self, *, filename:str = "config/db.ini", section:str = "postgresql"):
+        """
+        инициализация экземпляра класса, которая на вход принимает именнованные аргументы. filename - путь к конфигурационному файлу. section - в какой екции конфигурационного фала находяться данные.
+        config/db.examle - содержит пример конфигурационного файла
+        """
         self.conn= None
         self.cursor = None
         try:
@@ -30,6 +42,9 @@ class PGDB:
             exit()            
         
     def __del__(self):
+        """
+        перед удалением экземляа класса закрывает соединения с сервером
+        """
         if self.cursor:
             self.cursor.close()
             self.cursor = None
@@ -39,15 +54,22 @@ class PGDB:
         print("Отключено")
 
     def request(self, order:str) -> str:
+        """
+        отправка запроса к серверу и получение список ответов
+        """
         try:
             if not order : order = "SELECT version()"
             self.cursor.execute(order)
             return self.cursor.fetchall()
         except psycopg2.Error as error:
             print("Что то пошло не так...\n", error)
-    
+
     @staticmethod
     def config(fname:str ,sctn:str) -> dict:
+        """
+        статичный метод открытия конфигурационного файла и считывание данных из укаазнного сектора
+        на вход принимает путь к файлу и название секции для чтения.
+        """
         try:
             cfg = ConfigParser()
             cfg.read(fname)
@@ -55,7 +77,7 @@ class PGDB:
         except Exception as error:
             print(f"Не удалось получить параметры конфигурации\n {error}")
             exit()
-    
+
 if __name__ == "__main__":
     db = PGDB(filename= "config/db.ini", section = "postgresql")
     inputText = ""
