259a6315f286939ad5fb88d7185ba4b53e17cab3
Aleksandr644 <ivanov2-91@mail.ru>
2024-03-14 21:14:02 +0500

    correction of verification errors

--- main.py
+++ main.py
@@ -5,8 +5,8 @@ class RDBUGM():
         try:
             self.__db = connDB.PGDB(filename = "config/db.ini", section = "postgresql")
             if not self.verify_login(login):
-                self.__db.request(f"INSERT INTO person(login) values({login});")
-                self.__db.request(f"UPDATE person SET password=crypt({password}, gen_salt('bf')) WHERE login={login};")
+                self.__db.request(f"INSERT INTO person(login) values('{login}');")
+                self.__db.request(f"UPDATE person SET password=crypt('{password}', gen_salt('bf')) WHERE login='{login}';")
             elif self.verifi_password(login, password):
                 print("Доступ разрешен")
             else:
@@ -17,10 +17,16 @@ class RDBUGM():
             del self.__db
             self.__del__()
     def verify_login(self, login):
-        return self.__db.request(f"SELECT EXISTS(SELECT 1 FROM person WHERE login='{login}');")[0][0]
+        print("проверка логина")
+        b = self.__db.request(f"SELECT EXISTS(SELECT 1 FROM person WHERE login='{login}');")[0][0]
+        print("ответ ", b)
+        return b
 
     def verifi_password(self, login, password):
-        return self.__db.request(f"SELECT (password=crypt({password}, password) as pass_match FROM person WHERE login={login});")[0][0]
+        print("проверка пароля")
+        b = self.__db.request(f"SELECT (password=crypt('{password}', password)) AS pass_match FROM person WHERE login='{login}';")[0][0]
+        print("ответ ", b)
+        return b
 
     def __del__(self):
         del self.__db
